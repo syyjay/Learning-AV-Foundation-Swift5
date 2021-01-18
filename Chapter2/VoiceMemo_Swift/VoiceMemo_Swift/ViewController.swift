40b8313a955b3610730c08f001f2506d7ee2a9e8
丁次 <syyjay@sina.cn>
2021-01-18 16:35:56 +0800

    --	Chapter2 VoiceMemo

---
+++ Chapter2/VoiceMemo_Swift/VoiceMemo_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceMemo_Swift
+//
+//  Created by nathan on 2021/1/13.
+//  Copyright © 2021 nathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
